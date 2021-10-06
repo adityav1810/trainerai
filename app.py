acfac67708ade942b9f87a16d86b6ecbd696f13d
adityav1810 <adityavarshney99@gmail.com>
2021-10-06 17:07:38 +0530

    deploying frontend

--- app.py
+++ app.py
@@ -38,7 +38,3 @@ def video():
 @app.route('/workout_loader')
 def workout_loader():
     return render_template('workout_loader.html')
-if __name__ == '__main__':
-    app.debug = False
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, threaded=True)
