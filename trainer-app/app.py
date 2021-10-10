2574281e7e86693121ffc301d6da3f9dd460f56a
adityav1810 <adityavarshney99@gmail.com>
2021-10-10 09:32:00 +0530

    added client server

--- trainer-app/app.py
+++ trainer-app/app.py
@@ -38,7 +38,12 @@ def video():
 @app.route('/workout_loader')
 def workout_loader():
     return render_template('workout_loader.html')
+
+
+@app.route('/clientside')
+def clientside():
+    return render_template('client_video_stream.html')
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, threaded=True)
